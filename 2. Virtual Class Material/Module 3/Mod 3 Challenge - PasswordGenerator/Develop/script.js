c5021d2033ef78800e366a474e071fdde8262a4c
Emarie <amanda.evacampbell@gmail.com>
2022-01-23 21:13:02 -0500

    updated module 3 assignment - enemy robots

--- 2. Virtual Class Material/Module 3/Mod 3 Challenge - PasswordGenerator/Develop/script.js
+++ 2. Virtual Class Material/Module 3/Mod 3 Challenge - PasswordGenerator/Develop/script.js
@@ -1,5 +1,51 @@
 // Assignment code here
 
+// Prompt for password length - which must be between 8-128 character - ensure there is a prompt there if they put over or less than range and are prompted again if they don't put in the correct parameters - store the user input
+// 1. how many? if they put correct move to next part if they put incorrect go to the correction
+// 2. correction to put in correct amount
+
+// variables for generated password
+var uppercase = promptUpper
+
+//Prompt for number of characters
+var passwordLength = window.prompt("How many characters would you like your password to be?")
+
+if (passwordLength < 8 || passwordLength > 128){
+    window.alert("Length must be 8-128 characters")
+}
+
+//UPPERCASE
+// Prompt for uppercase 
+var promptUpper = window.prompt ("Would you like to include uppercase letters? YES or NO.")
+
+console.log(promptUpper)
+  //if they choose yes
+  if (promptUpper === "yes" || promptUpper === "YES") {
+    uppercase = true;
+  } else if (promptUpper === "no" || promptUpper === "NO") {
+    uppercase = true;
+  }
+
+  console.log(uppercase);
+
+
+
+
+// Prompt for lowercase
+
+// Prompt for numbers
+
+// Prompt for special characters
+
+// definitions for variables for generated password = uppercase = (enter data)
+
+// validate the input - must be between 8-128 and have at least one special character
+
+// generatePassword will be var uppercase = get lower random?
+// Link to generate button
+
+// Create a loop?
+
 
 // Get references to the #generate element // variable set up for the id=generate - this variable is for the below event listener  
 var generateBtn = document.querySelector("#generate");
@@ -15,6 +61,5 @@ function writePassword() {
 
 // Add event listener to generate button // when the user clicks the generate button, it will call the writepassword function above
 generateBtn.addEventListener("click", writePassword);
-
 
 
--- 2. Virtual Class Material/Module 3/Robot Gladiators Project/assets/js/main.js
+++ 2. Virtual Class Material/Module 3/Robot Gladiators Project/assets/js/main.js
@@ -4,14 +4,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -64,10 +61,26 @@ var fight = function() {
       window.alert("You need to choose a valid option. Try again!");
       }
     }
+
+    console.log(" Emarie now has " + playerMoney + " health remaining! ");
+    
+    console.log(arguments);
   };
    
 
-fight();
+//fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i])
+}
+
+
+
+/*for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}*/
 
 
 
